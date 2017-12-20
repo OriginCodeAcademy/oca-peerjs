65865ddc230f390da827d3d2d926c6daddb6272a
kyle <belfortk@gmail.com>
2017-12-20 09:40:54 -0800

    ready for deploy

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
-var port = process.env.PORT || 3000;
+var port =  6767 || process.env.PORT;
 app.listen(port);
 console.log('Listening on 6767');
