473c4229175b46c471e3d3e6b8d85911999babfb
kyle <belfortk@gmail.com>
2017-12-19 19:04:02 -0800

    deploy! + port

--- index.js
+++ index.js
@@ -17,5 +17,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
-app.listen(6767);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log('Listening on 6767');
